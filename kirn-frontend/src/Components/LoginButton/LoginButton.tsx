044b87f9e1f46101a19659375d43179b07fa2330
Kevin Prom <promkev@gmail.com>
2021-07-10 01:02:31 -0400

    Moved frontend to its own folder, and started appwrite function for user info

--- kirn-frontend/src/Components/LoginButton/LoginButton.tsx
+++ kirn-frontend/src/Components/LoginButton/LoginButton.tsx
@@ -4,7 +4,7 @@ import appwrite from '../../Constants/Appwrite';
 export default function LoginButton(props: ButtonProps) {
     const { REACT_APP_OAUTH2_REDIRECT_SUCCESS, REACT_APP_OAUTH2_REDIRECT_FAILURE } = process.env
     function login() {
-        appwrite.account.createOAuth2Session('discord', REACT_APP_OAUTH2_REDIRECT_SUCCESS, REACT_APP_OAUTH2_REDIRECT_FAILURE)
+        // appwrite.account.createOAuth2Session('discord', REACT_APP_OAUTH2_REDIRECT_SUCCESS, REACT_APP_OAUTH2_REDIRECT_FAILURE)
     }
     return (
         <Button {...props} variant="contained" color="secondary" onClick={login}>Log in with Discord</Button>
